c24f22132564bad13212f31e74afa95f5b2fa4bb
Martin Eriksson <martin@artilect.se>
2010-12-05 23:12:43 +0100

    Made the color hash a bit more readable.

--- colors.rb
+++ colors.rb
@@ -12,7 +12,16 @@ module Util
   
   module Color
   
-    @@colors = {:black=>0, :red=>1, :green=>2, :yellow=>3, :blue=>4, :magenta=>5, :cyan=>6, :white=>7}
+    @@colors = {
+      :black   => 0,
+      :red     => 1,
+      :green   => 2,
+      :yellow  => 3,
+      :blue    => 4,
+      :magenta => 5,
+      :cyan    => 6,
+      :white   => 7
+    }
 
     def paint string, options={}
       options[:color] && print("\033[3#{@@colors[options[:color]]}m")  
